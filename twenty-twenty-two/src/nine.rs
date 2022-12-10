0faf72e7669b54d210a0707f6e84ef760c52c299
George Nicoll <george.nicoll@gmail.com>
2022-12-10 00:44:37 +0000

    Tidy up code

--- twenty-twenty-two/src/nine.rs
+++ twenty-twenty-two/src/nine.rs
@@ -4,7 +4,6 @@ use substring::Substring;
 
 use crate::utils;
 
-
 const FILE_NAME: &str = "9/input.txt";
 
 pub fn _9a() -> Result<u64, Error>{
@@ -95,72 +94,73 @@ fn accumulate(state: State, head_move: Option<Move>) -> State {
 
 fn perform_moves(mut state: State, num: u8, horz_move: i16, vert_move: i16) -> State {
     for _ in 0..num {
-        let mut moved = false;
-        {
-            let first = state.positions.first_mut().unwrap();
-            first.x += horz_move;
-            first.y += vert_move;
-        }
+        apply_move_to_first(&mut state, horz_move, vert_move);
 
-        for position_index in 0..state.num_positions - 1 {
-            let (pos1_x, pos1_y) = {
-                let pos1 = state.positions.get(position_index).unwrap();
-                (pos1.x, pos1.y)
-            };
-            let pos2 = state.positions.get_mut(position_index + 1).unwrap();
+        let moved = apply_moves_to_chain(&mut state);
 
-            //decide if we need to move the second position
-            let horz_diff = (pos1_x - pos2.x).abs();
-            let vert_diff = (pos1_y - pos2.y).abs();
-
-            let horz_bump = if pos1_x > pos2.x {
-                1
-            } else {
-                -1
-            };
-            let vert_bump = if pos1_y > pos2.y {
-                1
-            } else {
-                -1
-            };
-
-            moved = match (horz_diff, vert_diff) {
-                (0, 0) => false,
-                (1, 0) => false,
-                (0, 1) => false,
-                (1, 1) => false,
-                (2, 2) => {
-                    pos2.x += horz_bump;
-                    pos2.y += vert_bump;
-                    true
-                }
-                (_, 0) => {
-                    pos2.x += horz_bump;
-                    true
-                }
-                (_, 1) => {
-                    pos2.x += horz_bump;
-                    pos2.y += vert_bump;
-                    true
-                },
-                (0, _) => {
-                    pos2.y += vert_bump;
-                    true
-                }
-                (1, _) => {
-                    pos2.x += horz_bump;
-                    pos2.y += vert_bump;
-                    true
-                }
-                _ => panic!("Unexpected difference between positions: ({}, {})", horz_diff, vert_diff)
-            };
-        }
         if moved {
             let last = state.positions.last().unwrap();
             state.visited_positions.insert(Position { x: last.x, y: last.y });
         }
     }
     state
+}
+
+fn apply_move_to_first(state: &mut State, horz_move: i16, vert_move: i16) {
+    let first = state.positions.first_mut().unwrap();
+    first.x += horz_move;
+    first.y += vert_move;
+}
+
+/// Apply moves to the chain, returning true if the last link had to move
+fn apply_moves_to_chain(state: &mut State) -> bool {
+    let mut moved = false;
+    for position_index in 0..state.num_positions - 1 {
+        let (pos1_x, pos1_y) = {
+            let pos1 = state.positions.get(position_index).unwrap();
+            (pos1.x, pos1.y)
+        };
+        let pos2 = state.positions.get_mut(position_index + 1).unwrap();
+
+        //decide if we need to move the second position
+        let horz_diff = (pos1_x - pos2.x).abs();
+        let vert_diff = (pos1_y - pos2.y).abs();
+        let (horz_bump, vert_bump) = calculate_bumps(pos1_x, pos1_y, pos2.x, pos2.y);
+
+        moved = match (horz_diff, vert_diff) {
+            (0, 0) | (1, 0) | (0, 1) | (1, 1) => false,
+            (2, 2) | (_, 1) | (1, _) => {
+                pos2.x += horz_bump;
+                pos2.y += vert_bump;
+                true
+            },
+            (_, 0) => {
+                pos2.x += horz_bump;
+                true
+            },
+            (0, _) => {
+                pos2.y += vert_bump;
+                true
+            },
+            _ => panic!("Unexpected difference between positions: ({}, {})", horz_diff, vert_diff)
+        };
+    }
+    moved
+}
+
+/// Calculates the (horizontal, vertical) bumps
+fn calculate_bumps(pos1_x: i16, pos1_y: i16, pos2_x: i16, pos2_y: i16) -> (i16, i16) {
+    let horz_bump = if pos1_x > pos2_x {
+        1
+    } else {
+        -1
+    };
+    let vert_bump = if pos1_y > pos2_y {
+        1
+    } else {
+        -1
+    };
+    (horz_bump, vert_bump)
 }
 
 fn reduce(state: State) -> u64 {
