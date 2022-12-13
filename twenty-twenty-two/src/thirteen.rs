533ba6f481c6d6f407170370e319f54dd1a97bc0
George Nicoll <george.nicoll@gmail.com>
2022-12-13 22:25:35 +0000

    Day13 switch to using Ordering

--- twenty-twenty-two/src/thirteen.rs
+++ twenty-twenty-two/src/thirteen.rs
@@ -117,17 +117,17 @@ fn in_right_order(items: &Vec<Item>) -> bool {
     }
     let left = items.get(0).unwrap();
     let right = items.get(1).unwrap();
-    compare_left_and_right(left, right) < 0
+    compare_left_and_right(left, right) == Ordering::Less
 }
 
 ///Compare the items, returns an integer where:
 /// result < 0, left is less than right
 /// result = 0, left is equal to right
 /// result > 0, left is greater than right
-fn compare_left_and_right(left: &Item, right: &Item) -> Value {
+fn compare_left_and_right(left: &Item, right: &Item) -> Ordering {
     match (left, right) {
         (Item::Val { val: left_val }, Item::Val{ val: right_val }) => {
-            left_val - right_val
+            left_val.cmp(&right_val)
         },
         (Item::List { vec: _vec }, Item::Val { val }) => {
             compare_left_and_right(left, &Item::List { vec: vec!(Item::Val{ val: *val }) })
@@ -140,19 +140,19 @@ fn compare_left_and_right(left: &Item, right: &Item) -> Value {
                 let left_item = left_vec.get(index).unwrap();
                 let right_item = right_vec.get(index);
                 if right_item.is_none() {
-                    return 1; //left is bigger than right - it has more elements
+                    return Ordering::Greater; //left is bigger than right - it has more elements
                 }
                 let comparison_result = compare_left_and_right(left_item, right_item.unwrap());
-                if comparison_result != 0 {
+                if comparison_result != Ordering::Equal {
                     return comparison_result;
                 }
             }
             //if we get here, all items were equal... if there are more on the right side then
             //it's in the correct order, or_else, we're equal
             if right_vec.len() > left_vec.len() {
-                return -1;
+                return Ordering::Less;
             }
-            0
+            Ordering::Equal
         },
     }
 }
@@ -197,16 +197,7 @@ fn reduce2(mut state: State) -> usize {
     state.items.push(divider1());
     state.items.push(divider2());
 
-    state.items.sort_by(|left, right| {
-        let comparison = compare_left_and_right(left, right);
-        if comparison < 0 {
-            return Ordering::Less;
-        };
-        if comparison == 0 {
-            return Ordering::Equal;
-        }
-        Ordering::Greater
-    });
+    state.items.sort_by(compare_left_and_right);
 
     let divider1 = divider1();
     let divider2 = divider2();
@@ -214,12 +205,12 @@ fn reduce2(mut state: State) -> usize {
     let mut second_index: Option<usize> = None;
     for (index, item) in state.items.iter().enumerate() {
         let one_based_index = index + 1;
-        if first_index.is_none() && compare_left_and_right(&divider1, item) == 0 {
+        if first_index.is_none() && compare_left_and_right(&divider1, item) == Ordering::Equal {
             first_index = Some(one_based_index);
-            // println!("**{}** {}", one_based_index, item);
-        } else if second_index.is_none() && compare_left_and_right(&divider2, item) == 0 {
+            // println!("**first: {}** {}", one_based_index, item);
+        } else if second_index.is_none() && compare_left_and_right(&divider2, item) == Ordering::Equal {
             second_index = Some(one_based_index);
-            // println!("**{}** {}", one_based_index, item);
+            // println!("**second: {}** {}", one_based_index, item);
         } else {
             // println!("{}", item);
         }
